016b20ff0a6593f4703021bdb3ea3abed21093b9
Gabriel Vieira <gabriel.goulart@tradetechnology.com.br>
2023-05-15 23:00:46 -0300

    first commit

---
+++ src/Core/Teste.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Core;
+
+class Teste
+{
+    public function __construct()
+    {
+        echo "Teste";
+    }
+
+    public function foo()
+    {
+        return '123';
+    }
+}
